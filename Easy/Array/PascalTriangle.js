8856748ec4ce83adb23ac44bf791a25e0e41386d
HiiragiMatu <matuyou0301@gmail.com>
2022-08-01 20:07:03 +0800

    problem: MergeSortedArray using while loop

---
+++ Easy/Array/PascalTriangle.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+const generate = function (numRows) {
+    const triangle = [];
+
+    if (numRows === 1) return triangle.push([1]);
+};
