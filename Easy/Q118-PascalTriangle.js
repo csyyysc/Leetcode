48fb47c43c202b880272879f0dcebf472122a345
HiiragiMatu <matuyou0301@gmail.com>
2023-08-26 09:45:28 +0800

    easy: Q118-PascalTriangle updated

--- Easy/Q118-PascalTriangle.js
+++ Easy/Q118-PascalTriangle.js
@@ -1,4 +1,5 @@
 /**
+ * @description Using comb function
  * @param {number} numRows
  * @return {number[][]}
  */
@@ -28,4 +29,24 @@ const comb = (u, l) => {
     }
 
     return num / den;
+}
+
+/**
+ * @description Refenrece Method1: Adding values from previous array;
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    const pascal = [];
+
+    for(let i = 0; i < numRows; i++) {
+        pascal[i] = [];
+        pascal[i][0] = 1;
+
+        for(let j = 1; j < i; j++) {
+            pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j];
+        }
+        pascal[i][i] = 1;
+    }
+    return pascal;
 }
