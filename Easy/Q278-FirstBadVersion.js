0e3b7a8af7cff3e4d1c0b78d4e7b2e77fcbd2497
HiiragiMatu <matuyou0301@gmail.com>
2023-09-05 18:22:32 +0800

    easy: Q278-FirstBadVersion

---
+++ Easy/Q278-FirstBadVersion.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let idx = 0;
+        for(let i = 1; i <= n ; i++) {
+            if(isBadVersion(i)) {
+                idx = i;
+                break;
+            }
+        }
+        return idx;
+    };
+};
