8d937495d56699351cfde2af61e809ee076fefe3
HiiragiMatu <matuyou0301@gmail.com>
2023-09-05 19:20:41 +0800

    easy: Q290-WordPattern

--- Easy/Q278-FirstBadVersion.js
+++ Easy/Q278-FirstBadVersion.js
@@ -28,3 +28,27 @@ var solution = function(isBadVersion) {
         return idx;
     };
 };
+
+/**
+ * @note Reference Method1
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+const solution = (isBadVersion) => {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let start = 1, end = n;
+        while(start < end) {
+            let mid = Math.floor((start + end) / 2);
+            if(isBadVersion(mid)) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    };
+};
