1b420443c4a111b2671936e76e19e84e15c995bc
Matsu Chen <sonamumatsu0223@gmail.com>
2023-09-11 00:06:21 +0800

    easy: update Q412-FizzBuzz

--- Easy/Q412-FizzBuzz.js
+++ Easy/Q412-FizzBuzz.js
@@ -20,4 +20,18 @@ var fizzBuzz = function(n) {
     return game;
 };
 
+var fizzBuzz = function(n) {
+    const game = [];
+
+    for(let i = 1; i <= n; i++) {
+        let str = "";
+        if(i % 3 === 0) str += "Fizz";
+        if(i % 5 === 0) str += "Buzz"; 
+        game.push(str.length > 0 ? str : `${i}`);
+    }
+
+    return game;
+};
+
+
 const fizzBuzz = (n) => new Array(n).fill(0).map((a, i) => (++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || '' + i);
