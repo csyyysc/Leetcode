d665247a45ad898adfca518c1953dfd230cea5b8
ChenMatsu <sonamumatsu0223@gmail.com>
2024-09-30 11:40:31 +0900

    Python/LeetCode75/Easy/SlidingWindow: Q643-MaximumAverageSubarrayI

---
+++ Python/Leetcode75/Easy/SlidingWindow/Q643-MaximumAverageSubarrayI.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        total = sum(nums[:k])
+        max_total = total
+
+        for i in range(1, len(nums) - k + 1):
+            total += nums[i + k - 1] - nums[i - 1]
+            max_total = max(max_total, total)
+
+        return max_total / k
