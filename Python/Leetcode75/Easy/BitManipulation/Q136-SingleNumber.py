2cb2800aa47977d621a4fe65ed56c4c70d43ad7b
ChenMatsu <sonamumatsu0223@gmail.com>
2024-10-05 17:38:44 +0900

    Python/LeetCode75/Easy/BitManipulation: Q136-SingleNumber, Q338-CountingBits

---
+++ Python/Leetcode75/Easy/BitManipulation/Q136-SingleNumber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        x = 0
+
+        for i in range(len(nums)):
+            x ^= nums[i]
+            
+        return x
+            
