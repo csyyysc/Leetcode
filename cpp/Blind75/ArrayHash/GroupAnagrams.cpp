32b46fce50fa40c18b4e01a924d21bf972b8ec1d
ChenMatsu <matuyou0301@gmail.com>
2025-04-23 14:24:00 +0900

    feat: 🎸 GroupAnagrams optimized approach added

--- cpp/Blind75/ArrayHash/GroupAnagrams.cpp
+++ cpp/Blind75/ArrayHash/GroupAnagrams.cpp
@@ -9,10 +9,35 @@ public:
             string sortedWord = word;
             sort(sortedWord.begin(), sortedWord.end());
             m[sortedWord].push_back(word);
-        } 
+        }
 
         vector<vector<string>> group;
         for (auto& pair : m) {
+            group.push_back(pair.second);
+        }
+
+        return group;
+    }
+
+    vector<vector<string>> groupAnagramsOpt(vector<string> &strs)
+    {
+        unordered_map<string, vector<string> m;
+
+        for (const auto&s : strs) {
+            vector<int> count(26, 0); 
+            for (char c : s) {
+                count[c - 'a']++;
+            }
+
+            string key = "";
+            for (int i = 0; i < 26; i++) {
+                key += ',' + to_string(count[i]);
+            }
+            m[key].push_back(s);
+        }
+
+        vector<vector<string>> group;
+        for (const auto& pair : m) {
             group.push_back(pair.second);
         }
 
